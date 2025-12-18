d83333ea178f9fc27669957b28a6fb4595ead15c
4Elentano <g.gareinov@fact.digital>
2025-12-18 13:48:30 +0500

    add button behavior

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,25 @@
 "use strict";
 
-let table = document.querySelector("tbody");
-console.log(table);
+let inputNumber = document.querySelector(".input__field");
+document.querySelector(".clean").addEventListener("click", () => {
+  inputNumber.value = "";
+});
 
-// window.addEventListener("load", () => {});
+document.querySelector(".percent").addEventListener("click", () => {
+  inputNumber.value %= +inputNumber.value;
+});
+
+document.querySelector(".plus").addEventListener("click", () => {
+  inputNumber.value = `${inputNumber.value}+`;
+});
+
+document.querySelector(".point").addEventListener("click", () => {
+  inputNumber.value += parseFloat(inputNumber.value);
+});
+
+// add number click event behavior
+for (let index = 0; index <= 9; index++) {
+  document.querySelector(`.number__${index}`).addEventListener("click", () => {
+    inputNumber.value += index;
+  });
+}
