d754b19b6ec120fe5544742a3df700ef5bdcb857
Gareynov Grigoriy <g.gareinov@fact.digital>
2025-12-18 21:53:21 +0500

    processing math operators

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,59 @@
 "use strict";
 
-let inputNumber = document.querySelector(".input__field");
-document.querySelector(".clean").addEventListener("click", () => {
-  inputNumber.value = "";
+const inputNumber = document.querySelector(".input__field");
+
+const operatorsList = new Object({
+  plus: "+",
+  subtraction: "-",
+  division: "/",
+  multiplication: "*",
 });
+
+//? Добавляем свой метод в прототип строк!
+String.prototype.replaceAt = function (index, char) {
+  return (
+    this.substring(0, index) + char + this.substring(index + 1, this.length)
+  );
+};
+
+//? Очистка поля
+document.querySelector(".clean").addEventListener("click", () => {
+  inputNumber.value = "0";
+});
+
+for (const item in operatorsList) {
+  document.querySelector(`.${item}`).addEventListener("click", () => {
+    addOperator(operatorsList[item]);
+  });
+}
+
+function addOperator(operator = "") {
+  if (
+    !Object.values(operatorsList).includes(
+      inputNumber.value[inputNumber.value.length - 1]
+    )
+  ) {
+    inputNumber.value += operator;
+  } else {
+    inputNumber.value = inputNumber.value.replaceAt(
+      inputNumber.value.length - 1,
+      operator
+    );
+  }
+}
 
 document.querySelector(".percent").addEventListener("click", () => {
-  inputNumber.value %= +inputNumber.value;
-});
-
-document.querySelector(".plus").addEventListener("click", () => {
-  inputNumber.value = `${inputNumber.value}+`;
+  //   inputNumber.value %= +inputNumber.value;
 });
 
 document.querySelector(".point").addEventListener("click", () => {
-  inputNumber.value += parseFloat(inputNumber.value);
+  //   inputNumber.value += parseFloat(inputNumber.value);
 });
 
 // add number click event behavior
 for (let index = 0; index <= 9; index++) {
   document.querySelector(`.number__${index}`).addEventListener("click", () => {
-    inputNumber.value += index;
+    if (inputNumber[inputNumber.value.length - 1] === "0")
+      inputNumber.value += index;
   });
 }
